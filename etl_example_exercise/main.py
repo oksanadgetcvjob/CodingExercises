e537eb685d36226a167e005bd2f331c9d650cba1
oksanadgetcvjob <111112908+oksanadgetcvjob@users.noreply.github.com>
2022-08-13 09:03:02 -0700

    task1

--- etl_example_exercise/main.py
+++ etl_example_exercise/main.py
@@ -1,1 +1,18 @@
 ### Start solution here
+import os
+import pandas as pd
+
+DATA = (('Input/data_source_1/sample_data.1.csv', ',', 'csv'),
+        ('Input/data_source_1/sample_data.2.dat', '|', 'dat'),
+        ('Input/data_source_2/sample_data.3.dat', ',', 'dat'))
+
+import_function = {'csv': pd.read_csv, 'dat': pd.read_table}
+
+result = pd.DataFrame()
+for i, source in enumerate(DATA):
+    df = import_function[source[2]](source[0], sep=source[1])
+    df['source'] = i
+    result = pd.concat([result, df])
+if not os.path.isdir('Output'):
+    os.mkdir('Output')
+result.to_csv(r'Output/consolidated_output.1.csv', header=True)
